2c99ea2e0ce205b49d70cd00e6587557f2636045
pangeran151 <49519527+pangeran151@users.noreply.github.com>
2019-04-26 19:40:35 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_104346270667120";
+$raw_uid="ee4e5a9b-dd2a-4561-9ffb-6a1e1e5bf5bc";
+$user_name="Smartfren+Smart";
+$invite_code="FTd4cnnCi";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="e865fcb8b0c9cc13";
+$device_type="Xiaomi++Redmi+Note+4";
+
+
+?>
